fac81d096c97239cbe1eea559e42170356033699
Katy Thompson <katimation@gmail.com>
2023-10-30 11:11:06 -0500

    added comments to html and javascript and created README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # work-day-scheduler
+
+## Description 
+
+This project was completed so users could have a scheduler for their work day. 
+
+## Installation
+
+N/A
+
+## Usage
+
+Users can input events into a workday scheduler and save them to their local storage. The scheduler will display whether a time slot is in the past, present, or future. 
+
+## Credits 
+
+Starter Code : https://github.com/coding-boot-camp/crispy-octo-meme
+
+## License
+
+MIT License
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// All code is wrapped in this function so it doesn't run until the browser is finished rendering the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
+
+    // Save button click event listener. It saves both the input text and the hour slot.
     $(".saveBtn").on("click", function(){
         var text = $(this).siblings(".description").val();
         console.log(text);
@@ -17,19 +10,10 @@ $(function () {
         window.localStorage.setItem(time, text);
 
     })
-    $(".deleteBtn").click(function(){
-        var text = $(this).siblings(".description").val("");
-        var time = $(this).siblings(".hour").text();
-        localStorage.setItem(time, JSON.stringify(text));
-    })
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
+   
+    // Adds a past, present, or future class to each time block, based on the current time and the schedule time slots. 
     function ppfColor(){
-        var currentHour = dayjs().format("h");
+        var currentHour = dayjs().format("hh");
         $(".description").each(function(){
             var scheduleTime = parseInt($(this).parent().attr("id"));
 
@@ -49,22 +33,20 @@ $(function () {
         });
     }
     ppfColor();
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-   
-    $("#-3 .description").val(localStorage.getItem("9AM"));
-    $("#-2 .description").val(localStorage.getItem("10AM"));
-    $("#-1 .description").val(localStorage.getItem("11AM"));
-    $("#0 .description").val(localStorage.getItem("12PM"));
-    $("#1 .description").val(localStorage.getItem("1PM"));
-    $("#2 .description").val(localStorage.getItem("2PM"));
-    $("#3 .description").val(localStorage.getItem("3PM"));
-    $("#4 .description").val(localStorage.getItem("4PM"));
-    $("#5 .description").val(localStorage.getItem("5PM"));
+
+    // Retrieves any input saved in the individual time slot input fields, so they will be there on reload. 
+    $("#9 .description").val(localStorage.getItem("9AM"));
+    $("#10 .description").val(localStorage.getItem("10AM"));
+    $("#11 .description").val(localStorage.getItem("11AM"));
+    $("#12 .description").val(localStorage.getItem("12PM"));
+    $("#13 .description").val(localStorage.getItem("1PM"));
+    $("#14 .description").val(localStorage.getItem("2PM"));
+    $("#15 .description").val(localStorage.getItem("3PM"));
+    $("#16 .description").val(localStorage.getItem("4PM"));
+    $("#17 .description").val(localStorage.getItem("5PM"));
    
 
-    // TODO: Add code to display the current date in the header of the page.
+    // Diplays the current date and time in the header of the page. 
     var today = dayjs();
     $("#currentDay").text("It is currently " + today.format("MMM D, YYYY h:mm A"));
   });
